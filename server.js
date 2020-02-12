3bed8def4b15e0577fe0f4d935266f184ef3c718
sgharai <sofiagharaibeh@gmail.com>
2020-02-12 16:51:21 -0500

    adds women

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.listen(port, () => console.log("Hi Sofia~"))
 let schema = buildSchema(`
     type Query {
         hello: String
-        women: User
+        women:
     }
 
     type User {
@@ -42,33 +42,33 @@ let root = {
     women: () => women
 }
 
-let women = {
-    whitney : {
+let women = [
+     {
         "_id": "5e446ccdeeda84bfeccb0a9e",
         "name": "Whitney Rojas",
         "gender": "female"
       },
-     harriet: {
+      {
         "_id": "5e446ccd2fa2bb2c21185453",
         "name": "Harriet Velazquez",
         "gender": "female"
       },
-     jewell: {
+      {
         "_id": "5e446ccd6a47a8360a7f74ce",
         "name": "Jewell Hines",
         "gender": "female"
       },
-      saundra: {
+      {
         "_id": "5e446ccd0add1868d72cf096",
         "name": "Saundra Payne",
         "gender": "female"
       },
-      bettye : {
+      {
         "_id": "5e446ccd135fa659817bb7b6",
         "name": "Bettye Wiggins",
         "gender": "female"
       }
-}
+    ]
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
