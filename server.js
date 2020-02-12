7ce12d35bcc1a6238d2156283666107e38520501
sgharai <sofiagharaibeh@gmail.com>
2020-02-12 16:41:26 -0500

    adds women

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ let schema = buildSchema(`
     type Query {
         hello: String
     }
+
+    type User {
+        _id: ID
+        name: String
+        gender: String
+    }
 `);
 
 // let db = {
@@ -30,7 +36,38 @@ let schema = buildSchema(`
 //     }
 // }
 
-let root = { hello: () => "Hello world!"}
+let root = { 
+    hello: () => "Hello world!",
+    women: () => women
+}
+
+let women = {
+    whitney : {
+        "_id": "5e446ccdeeda84bfeccb0a9e",
+        "name": "Whitney Rojas",
+        "gender": "female"
+      },
+     harriet: {
+        "_id": "5e446ccd2fa2bb2c21185453",
+        "name": "Harriet Velazquez",
+        "gender": "female"
+      },
+     jewell: {
+        "_id": "5e446ccd6a47a8360a7f74ce",
+        "name": "Jewell Hines",
+        "gender": "female"
+      },
+      saundra: {
+        "_id": "5e446ccd0add1868d72cf096",
+        "name": "Saundra Payne",
+        "gender": "female"
+      },
+      bettye : {
+        "_id": "5e446ccd135fa659817bb7b6",
+        "name": "Bettye Wiggins",
+        "gender": "female"
+      }
+}
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
